66b4d2766ed3fa43b098339de37403d71c346b9a
Martín Gimenez <71200966+martingabrielgimenez@users.noreply.github.com>
2024-12-09 08:25:56 -0300

    Se agregó port db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,6 +1,7 @@
 <?php
 class Database {
     private $host = 'localhost';
+    private $port = 3306;
     private $db_name = 'inventario';
     private $username = 'root';
     private $password = '';
@@ -9,7 +10,11 @@ class Database {
     public function connect() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=$this->host;port=$this->port;dbname=$this->db_name", 
+                $this->username, 
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
